287a7bdc7a4e09eae5b2d435faf4ad1a0ecc02b5
madhusuthanan24 <madhusuthananp24cs@srishakthi.ac.in>
2026-06-22 21:30:36 +0530

    345. Reverse Vowels of a String

---
+++ Easy/Reverse Vowels of a String.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseVowels(String s) {
+        int left=0;
+        int right=s.length()-1;
+        int temp=0;
+        while(left<right){
+            char c=s.charAt(left).toLowerCase();
+            char d=s.charAt(right).toLowerCase();
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'&&d=='a'||d=='e'||d=='i'||d=='o'||d=='u'){
+                
+            }
+            
+        }
+    }
+}
