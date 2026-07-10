2627137cfb625cb68cc8355a574c6a5e4f0f6f81
madhusuthanan24 <madhusuthananp24cs@srishakthi.ac.in>
2026-07-10 11:25:55 +0530

    63. Unique Paths II(non linear

---
+++ Dynamic programming/Unique Paths II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] o) {
+        int dp[][]=new int[o.length][o[0].length];
+        dp[0][0]=1;
+        for(int  i=0;i<o.length;i++){
+            for(int j=0;j<o[0].length;j++){
+                if(o[i][j]==0){
+                if(i>0){
+                    dp[i][j]+=dp[i-1][j];
+                }
+                if(j>0){
+                    dp[i][j]+=dp[i][j-1];
+                }
+                }
+                else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return dp[o.length-1][o[0].length-1];
+    }
+}
